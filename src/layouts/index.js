374e3a51abd4c78a5241bb73b0b04ccbe09b11b0
tianyanggu <tianyanggu@hotmail.com>
2018-07-15 15:37:00 -0700

    Plugins, Sidebar, and Header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
+//import Media from 'react-media'
 
 import Header from '../components/header'
+import Sidebar from '../components/sidebar'
 import './index.css'
+import '../styles/layout-override.css'
 
 const Layout = ({ children, data }) => (
   <div>
