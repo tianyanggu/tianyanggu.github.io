8bf9e00460e3da32f5ad8504e14a5416592f875c
tianyanggu <tianyanggu@hotmail.com>
2018-07-15 16:34:10 -0700

    Test

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "C:/Users/Tianyang/Documents/GitHub/tianyanggu.github.io/src/layouts/index.js"
+  import data from "C:\\Users\\Tianyang\\Documents\\GitHub\\tianyanggu.github.io\\.cache\\json\\layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
