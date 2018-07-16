5845f2a82d296d321af70d9085c335c88796fa7a
tianyanggu <tianyanggu@hotmail.com>
2018-07-15 17:03:42 -0700

    Revert "Test"
    This reverts commit 8bf9e00460e3da32f5ad8504e14a5416592f875c.

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "C:/Users/Tianyang/Documents/GitHub/tianyanggu.github.io/src/layouts/index.js"
-  import data from "C:\\Users\\Tianyang\\Documents\\GitHub\\tianyanggu.github.io\\.cache\\json\\layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
